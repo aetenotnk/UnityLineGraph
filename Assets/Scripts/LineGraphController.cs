679d733c57bb27737f741e4cc80ab35ed6f0e69d
yamazaki yuta <aetenotnk@ezweb.ne.jp>
2019-03-22 14:01:59 +0900

    プロジェクト作成

---
+++ Assets/Scripts/LineGraphController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineGraphController : MonoBehaviour
+{
+    private RectTransform viewport;
+
+    private void Awake()
+    {
+
+    }
+}
