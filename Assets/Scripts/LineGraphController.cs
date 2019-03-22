b6ea483f71243d5be042ae2ea6ce3f3591feb80b
yamazaki yuta <aetenotnk@ezweb.ne.jp>
2019-03-22 14:48:28 +0900

    軸の初期化を実装

--- Assets/Scripts/LineGraphController.cs
+++ Assets/Scripts/LineGraphController.cs
@@ -5,10 +5,44 @@ using UnityEngine.UI;
 
 public class LineGraphController : MonoBehaviour
 {
+    // グラフを表示する範囲
     private RectTransform viewport;
+    // グラフの要素を配置するContent
+    // グラフの要素はグラフの点、ライン
+    private RectTransform content;
+
+    private GameObject xAxis;
+    private GameObject yAxis;
 
     private void Awake()
     {
+        viewport = this.transform.Find("Viewport") as RectTransform;
+        content = viewport.Find("Content") as RectTransform;
+        xAxis = this.transform.Find("X Axis").gameObject;
+        yAxis = this.transform.Find("Y Axis").gameObject;
+    }
 
+    private void Start()
+    {
+        InitializeAxis();
+    }
+
+    /// <summary>
+    /// X軸、Y軸の位置、サイズを設定
+    /// </summary>
+    private void InitializeAxis()
+    {
+        Vector2 origin = content.position;
+        RectTransform xAxisTransform =
+            xAxis.GetComponent<RectTransform>();
+        RectTransform yAxisTransform =
+            yAxis.GetComponent<RectTransform>();
+
+        xAxis.transform.position = origin;
+        yAxis.transform.position = origin;
+        xAxisTransform.sizeDelta =
+            new Vector2(viewport.rect.width, xAxisTransform.sizeDelta.y);
+        yAxisTransform.sizeDelta =
+            new Vector2(viewport.rect.height, yAxisTransform.sizeDelta.y);
     }
 }
